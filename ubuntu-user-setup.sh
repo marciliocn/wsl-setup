bee3f1151aafa13980968a6cd10de321cc0487e5
Marcílio Nascimento <marcilio.mcn@gmail.com>
2021-01-23 10:13:11 -0300

    Diversas Melhorias
    - Troca de repositório do Ctrl+Z
    \n- Adicionado mudança de permissão em ~/.ssh
    \n- Adicionado instalação do ZSH for Humans

--- ubuntu-user-setup.sh
+++ ubuntu-user-setup.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
+# Define permissões de escrita em ~/.ssh apenas para $USUARIO
+[ ! -d ~/.ssh ] && mkdir ~/.ssh
+chmod 700 ~/.ssh
+
+# Se exitir arquivos na pasta, alterar permissão para melhorar segurança
+# chmod 600 ~/.ssh/*
+
 # ASDF: gerenciador de pacotes universal
 # https://asdf-vm.com/
 # Instala apenas se não existir pasta
 if [ ! -d ~/.asdf ]
 then
-	git clone https://github.com/asdf-vm/asdf ~/.asdf --branch v0.8.0
-	echo '\n. ~/.asdf/asdf.sh\n' >> ~/.zshrc
+    git clone https://github.com/asdf-vm/asdf ~/.asdf --branch v0.8.0
+    echo '\n. ~/.asdf/asdf.sh\n' >> ~/.zshrc
 fi
 
 # Adiciona ao ZSH atalho Ctrl+Z para controle de Jobs
-curl -sL https://raw.githubusercontent.com/Peeja/ctrl-zsh/master/ctrl-zsh.plugin.zsh > ~/.ctrlz
+# curl -sL https://raw.githubusercontent.com/Peeja/ctrl-zsh/master/ctrl-zsh.plugin.zsh > ~/.ctrlz
+curl -sL https://raw.githubusercontent.com/mdumitru/fancy-ctrl-z/master/fancy-ctrl-z.zsh > ~/.ctrlz
 cat ~/.ctrlz >> ~/.zshrc && rm ~/.ctrlz
 echo '\n' >> ~/.zshrc
 
---
+++ zsh4humans.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Instala o ZSH for Humans
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v4/install)"
+
