c3105bb2dd04783f94f7853448797aee610387a5
Marcílio Nascimento <marcilio.mcn@gmail.com>
2021-01-25 12:04:40 -0300

    Organização do arquivo e adição de pacote para XML

--- ubuntu-user-setup.sh
+++ ubuntu-user-setup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# Adiciona ao ZSH atalho Ctrl+Z para controle de Jobs
+# curl -sL https://raw.githubusercontent.com/Peeja/ctrl-zsh/master/ctrl-zsh.plugin.zsh > ~/.ctrlz
+curl -sL https://raw.githubusercontent.com/mdumitru/fancy-ctrl-z/master/fancy-ctrl-z.zsh > ~/.ctrlz
+cat ~/.ctrlz >> ~/.zshrc && rm ~/.ctrlz
+echo '\n' >> ~/.zshrc
+
 # Define permissões de escrita em ~/.ssh apenas para $USUARIO
 [ ! -d ~/.ssh ] && mkdir ~/.ssh
 chmod 700 ~/.ssh
-
-# Adiciona Plugin Manager Vim-Plug para o NeoVim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Se exitir arquivos na pasta, alterar permissão para melhorar segurança
 # chmod 600 ~/.ssh/*
@@ -19,13 +22,13 @@ then
     echo '\n. ~/.asdf/asdf.sh\n' >> ~/.zshrc
 fi
 
-# Adiciona ao ZSH atalho Ctrl+Z para controle de Jobs
-# curl -sL https://raw.githubusercontent.com/Peeja/ctrl-zsh/master/ctrl-zsh.plugin.zsh > ~/.ctrlz
-curl -sL https://raw.githubusercontent.com/mdumitru/fancy-ctrl-z/master/fancy-ctrl-z.zsh > ~/.ctrlz
-cat ~/.ctrlz >> ~/.zshrc && rm ~/.ctrlz
-echo '\n' >> ~/.zshrc
-
 # ASDF: NodeJS
 # asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 # bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+
+# Adiciona Plugin Manager Vim-Plug para o NeoVim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# [Opcional] Instala pacote para formação de arquivos XML
+sudo apt install -y libxml2-utils
 
